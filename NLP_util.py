16bf633bd412762a551151a0598b9e8e5edbe43f
omid-mohebi <omohebi12@gmail.com>
2024-11-24 22:14:25 +0330

    creating tokenixe draft

--- NLP_util.py
+++ NLP_util.py
@@ -1,4 +1,25 @@
-import numpy as np
+################################
+#import nltk
+#nltk.download('stopwords')
+#nltk.download('wordnet')
+################################
 
-def tokenize(data):
-    pass
+
+import numpy as np
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+
+def tokenize(data, is_pandas=True, want_lemmatize=True):
+    stop_words = set(stopwords.words('english'))
+    lemmatizer = WordNetLemmatizer()
+    tokens = []
+    for text in data:
+        tokenized_text = [
+            lemmatizer.lemmatize(word) if want_lemmatize else word
+            for word in text.split() 
+            if (word not in stop_words) and (len(word) > 3)
+        ]
+        tokens.append(tokenized_text)
+    return tokens
+
